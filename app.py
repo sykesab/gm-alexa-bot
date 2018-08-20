6e2dff44313ed67ac4eb462b8d99de8d58fbb023
Aaron Sykes <absykes1997@gmail.com>
2018-08-19 23:10:46 -0400

    changed name to correct one and cleaned up comments

--- app.py
+++ app.py
@@ -9,18 +9,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-#group_id = '42721784'
-
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
 
-    if data['name'] != 'test-alexa':
+    if data['name'] != 'Alexa':
         if data['text'][:5].lower() == 'alexa':
             time.sleep(2)
             msg = 'ɴᴏᴡ ᴘʟᴀʏɪɴɢ: Despacito ───────────────⚪─────────────────── ◄◄⠀▐▐ ⠀►►⠀⠀ ⠀ 1:17 / 3:48 ⠀ ───○ 🔊⠀ ᴴᴰ ⚙ ❐ ⊏⊐'
             send_message(msg)
             despacito = 'https://youtu.be/kJQP7kiw5Fk?t=21s'
+            time.sleep(3)
             send_message(despacito)
 
     return "ok", 200
@@ -34,26 +33,3 @@ def send_message(msg):
     }
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
-#
-#     data = request.get_json()
-#
-#     if data['text'][:5] == 'Alexa':
-#         if data['name'] != 'test-alexa':
-#             time.sleep(5)
-#             send_message('yes')
-#     else:
-#         send_message('no call')
-#
-#     return "ok", 200
-#
-# def send_message(msg):
-#     url = 'https://api.groupme.com/v3/groups/bots/post'
-#
-#     data = {
-#         'bot_id' : bot_id,
-#         'text' : msg,
-#     }
-#
-#     rqst = Request(url, urlencode(data).encode())
-#     print(rqst)
-#     json = urlopen(rqst).read().decode()
