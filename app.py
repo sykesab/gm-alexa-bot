cc50d5e57cc7263cababb83b925f35a6b2a70b20
Aaron Sykes <absykes1997@gmail.com>
2018-08-19 22:54:23 -0400

    reverting to basic test version

--- app.py
+++ app.py
@@ -9,30 +9,47 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-bot_id = '057d3858182e8a31f234c38eaa'
-group_id = '42721784'
+#group_id = '42721784'
 
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
 
-    if data['text'][:5] == 'Alexa':
-        if data['name'] != 'test-alexa':
-            time.sleep(5)
-            send_message('yes')
-    else:
-        send_message('no call')
+    if data['name'] != 'test-alexa':
+        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+        send_message(msg)
 
     return "ok", 200
 
 def send_message(msg):
-    url = 'https://api.groupme.com/v3/groups/bots/post'
+    url = 'https://api.groupme.com/v3/bots/post'
 
     data = {
-        'bot_id' : bot_id,
-        'text' : msg,
+        'bot_id'    : os.getenv('GROUPME_BOT_ID'),
+        'text'      : msg
     }
-
-    rqst = Request(url, urlencode(data).encode())
-    print(rqst)
-    json = urlopen(rqst).read().decode()
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+#
+#     data = request.get_json()
+#
+#     if data['text'][:5] == 'Alexa':
+#         if data['name'] != 'test-alexa':
+#             time.sleep(5)
+#             send_message('yes')
+#     else:
+#         send_message('no call')
+#
+#     return "ok", 200
+#
+# def send_message(msg):
+#     url = 'https://api.groupme.com/v3/groups/bots/post'
+#
+#     data = {
+#         'bot_id' : bot_id,
+#         'text' : msg,
+#     }
+#
+#     rqst = Request(url, urlencode(data).encode())
+#     print(rqst)
+#     json = urlopen(rqst).read().decode()
